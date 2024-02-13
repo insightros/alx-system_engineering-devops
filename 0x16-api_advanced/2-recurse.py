066ee21f934cf5fed66902c893b9d7151bf5b767
xeno <xeno@xeno.0.2.15>
2024-02-13 18:38:48 +0100

    valentine

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-"""Function to query a list of all hot posts on a given Reddit subreddit."""
+"""
+ueries the Reddit API and returns a list containing the titles
+of all hot articles for a given subreddit.
+"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "insightros"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    lists all top hot articles
+    """
+    headers = {"User-Agent": "MyRedditClient/1.0"}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if after:
+        url += "&after={}".format(after)
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
         return None
-
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    data = response.json()
+    posts = data["data"]["children"]
+    if not posts:
+        return hot_list
+    titles = [post["data"]["title"] for post in posts]
+    hot_list.extend(titles)
+    next_page = data["data"]["after"]
+    if next_page:
+        return recurse(subreddit, hot_list, next_page)
+    else:
+        return hot_list
