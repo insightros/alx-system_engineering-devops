7c0d40c6c5dbb01090b9773aed90761880a7ef3c
xeno <xeno@xeno.0.2.15>
2024-02-13 18:40:54 +0100

    api advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""
-ueries the Reddit API and returns a list containing the titles
-of all hot articles for a given subreddit.
-"""
+"recurse"
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """
-    lists all top hot articles
-    """
     headers = {"User-Agent": "MyRedditClient/1.0"}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     if after:
