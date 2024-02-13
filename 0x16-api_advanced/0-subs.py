b780ea27ef48f44606aa50f411b4f712ad1e30d4
xeno <xeno@xeno.0.2.15>
2024-02-13 18:41:27 +0100

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,10 +4,8 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    headers = {"User-Agent": "MyRedditClient/1.0"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "insightros"
-    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
