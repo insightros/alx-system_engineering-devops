9392509e4f7548ddd9541b6493501b2ec97bbdec
xeno <xeno@xeno.0.2.15>
2024-02-13 14:29:06 +0100

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,18 +2,33 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
+
+    url = f"https://api.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "your_unique_user_agent"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+        if data['kind'] != 't5':
+            raise ValueError("Invalid subreddit or incorrect endpoint")
+
+        return data['data']['subscribers']
+
+    except (requests.exceptions.RequestException, ValueError) as e:
+        print(f"Error retrieving subscriber count: {e}")
         return 0
 
+
 if __name__ == "__main__":
-    subreddit = input("Enter subreddit name: ")
-    print(number_of_subscribers(subreddit))
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        subscribers = number_of_subscribers(subreddit)
+        print(f"Number of subscribers for r/{subreddit}: {subscribers}")
