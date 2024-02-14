4e1004439cb96eb884e9d330021dbdae65512154
xeno <xeno@xeno.0.2.15>
2024-02-14 15:27:37 +0100

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,19 @@
-#!/usr/bin/python3
-"Function to query subscribers on a given Reddit subreddit."
 import requests
 
 
 def number_of_subscribers(subreddit):
-    headers = {"User-Agent": "MyRedditClient/1.0"}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'my-reddit-api-client'}
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        if data['kind'] == 't5':
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.exceptions.HTTPError as err:
+        if response.status_code == 404:
+            return 0
+        else:
+            raise err
