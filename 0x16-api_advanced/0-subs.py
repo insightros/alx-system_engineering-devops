6712d377cd048de293b3df209107864d16cb1244
xeno <xeno@xeno.0.2.15>
2024-02-14 15:16:55 +0100

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,25 @@
-#!/usr/bin/python3
-"Function to query subscribers on a given Reddit subreddit."
 import requests
 
 
 def number_of_subscribers(subreddit):
-    headers = {"User-Agent": "MyRedditClient/1.0"}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyRedditClient'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        if 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            return 0
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+
+if __name__ == "__main__":
+    subreddit = input("Enter a subreddit: ")
+    subscribers = number_of_subscribers(subreddit)
+    if subscribers == 0:
+        print("nonexisting subreddit")
+    else:
+        print("existing Subreddit")
