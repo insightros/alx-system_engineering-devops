9ddc1a55822bcbd46344aea84f70c7a45ef7900c
xeno <xeno@xeno.0.2.15>
2024-02-14 15:20:20 +0100

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,10 +4,13 @@ import requests
 def number_of_subscribers(subreddit):
     headers = {"User-Agent": "MyRedditClient/1.0"}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json().get("data", {})
-        return data.get("subscribers", 0)
+        if "subscribers" in data:
+            return data["subscribers"]
+        else:
+            return 0
     else:
         return 0
