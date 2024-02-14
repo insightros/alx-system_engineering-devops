c925c6c376a021e0002a42b37e79084d9c03dea0
xeno <xeno@xeno.0.2.15>
2024-02-14 15:18:29 +0100

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,24 +2,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    headers = {"User-Agent": "MyRedditClient/1.0"}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'MyRedditClient'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        if 'subscribers' in data['data']:
-            return data['data']['subscribers']
-        else:
-            return 0
+        data = response.json().get("data", {})
+        return data.get("subscribers", 0)
     else:
         return 0
-
-
-if __name__ == "__main__":
-    subreddit = input("Enter a subreddit: ")
-    subscribers = number_of_subscribers(subreddit)
-    if subscribers == 0:
-        print("nonexisting subreddit")
-    else:
-        print("existing Subreddit")
